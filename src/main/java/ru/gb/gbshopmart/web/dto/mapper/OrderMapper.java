c674580015afac5ca9da1982b177778b7b5041c1
Artem Kropotov <aakropotov@alfabank.ru>
2022-04-15 21:00:43 +0300

    lesson 7 add order

---
+++ src/main/java/ru/gb/gbshopmart/web/dto/mapper/OrderMapper.java
@@ -0,0 +1,16 @@
+package ru.gb.gbshopmart.web.dto.mapper;
+
+import org.mapstruct.Context;
+import org.mapstruct.Mapper;
+import ru.gb.gbapi.order.dto.OrderDto;
+import ru.gb.gbshopmart.dao.CategoryDao;
+import ru.gb.gbshopmart.dao.ManufacturerDao;
+import ru.gb.gbshopmart.entity.Order;
+
+@Mapper(uses = {ProductMapper.class, ManufacturerMapper.class})
+public interface OrderMapper {
+
+    Order toOrder(OrderDto orderDto, @Context ManufacturerDao manufacturerDao, @Context CategoryDao categoryDao);
+
+    OrderDto toOrderDto(Order order);
+}
